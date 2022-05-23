73090cd8051fcf12f31329a9f9653530f0023f9a
Alejo Dieguez <alejodieguez2012@gmail.com>
2022-05-23 00:56:52 -0300

    updated app.js
    minor changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esta es mi nueva aplicacion hecha con React!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Clickea aqui para aprender mas, comencemos!
         </a>
       </header>
     </div>
