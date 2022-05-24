f2f3d388c3657a8b37a9ae7becddd540c5494d5d
Alejo Dieguez <alejodieguez2012@gmail.com>
2022-05-23 21:56:01 -0300

    feat: nav component created

---
+++ src/components/navComponent/NavBar.jsx
@@ -0,0 +1,25 @@
+import icon from './logo-navbar.jpg'
+import './main.css'
+function NavBar() {
+    return (
+        <header id="header-section">
+            <nav id="nav-section">
+                <img src={icon} alt="Casa D Velas Logotipo"/>
+                <input type="checkbox" id="hmb-menu" className="hmb-menu"/>
+                <label htmlFor="hmb-menu" className="hmb-label">
+                    <span className="bar bar1"/>
+                    <span className="bar bar2"/>
+                    <span className="bar bar3"/>
+                </label>
+                <ul className="nav-ul">
+                    <li><a>Inicio</a></li>
+                    <li><a>Productos</a></li>
+                    <li><a>Carrito</a></li>
+                    <li><a>Contacto</a></li>
+                </ul>
+            </nav>
+            <div className="spacer-bar"/>
+        </header>
+    )
+}
+export default NavBar
