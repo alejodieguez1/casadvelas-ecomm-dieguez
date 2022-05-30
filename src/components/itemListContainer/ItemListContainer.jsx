da686042d6a7701b8206a5af7f738cc61706ed92
Alejo Dieguez <alejodieguez2012@gmail.com>
2022-05-30 17:23:33 -0300

    removed empty code

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import './main.css'
 export default function ItemListContainer({greeting}) {
-  return (<>
+  return (
   <section id='main-section'>
     <p>Esto es el item list container</p>
     <button className='greet-btn' type='button' onClick={() => greeting()}>Click me!</button>
   </section>
-  </>
   )
 }
