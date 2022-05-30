47ff0933dd799469b131c539a27d2629260ed2c9
Alejo Dieguez <alejodieguez2012@gmail.com>
2022-05-30 20:47:46 -0300

    feat count buttons created

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer greeting={saludar}/>
+      <ItemListContainer/>
     </div>
   );
 }
--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './main.css'
-export default function ItemListContainer({greeting}) {
+export default function ItemListContainer() {
   return (
   <section id='main-section'>
-    <p>Esto es el item list container</p>
-    <button className='greet-btn' type='button' onClick={() => greeting()}>Click me!</button>
+    <p>Este es un producto</p>
+    <p>El stock del producto es: </p>
+    <div className='count-container'>
+        <button type='button' className='product-btn'>-</button>
+        <p>A</p>
+        <button type='button' className='product-btn'>+</button>
+        <button type='button' className='product-btn'>Add to cart</button>
+    </div>
   </section>
   )
 }
