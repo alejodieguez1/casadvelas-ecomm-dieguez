d7e8ae08a2a8edeb4cb54b3e3f81bceb45ac1592
Alejo Dieguez <alejodieguez2012@gmail.com>
2022-06-03 14:43:00 -0300

    imported woff2 font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './assets/fonts/RecoletaAlt-Medium.woff'
+import './assets/fonts/RecoletaAlt-Medium.woff2'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
